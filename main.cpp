70d271652da2bb9a9e966b1344a51cf8e58719eb
RyanDalmas <rdalm001@ucr.edu>
2022-04-28 21:38:02 -0700

    Added initial files and tested setup for project 1

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#ifndef __MAIN_CPP__
+#define __MAIN_CPP__
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    cout << "Hello world \n";
+    //system("pause");
+    return 0;
+}
+
+#endif
